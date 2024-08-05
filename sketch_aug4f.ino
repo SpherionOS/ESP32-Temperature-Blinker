d6de6e70133d73c42a6e858620c6e96f14de5cf1
SpherionOS <122868668+SpherionOS@users.noreply.github.com>
2024-08-05 11:11:05 +1000

    Add files via upload

---
+++ sketch_aug4f.ino
@@ -0,0 +1,78 @@
+
+#include <ezButton.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+
+#define LED_BUILTIN 18
+
+// Replace with your network credentials
+const char* ssid = "S-TFP";
+const char* password = "DieInTheDark.12";
+ezButton button(22);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial.println();
+  // Initialize Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  button.loop();
+  int btnState = button.getState();
+  if (btnState == 0) {
+       WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    // set secure client without certificate
+    client->setInsecure();
+    //create an HTTPClient instance
+    HTTPClient https;
+
+    //Initializing an HTTPS communication using the secure client
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, "https://wttr.in/Brisbane?format=%t")) {  // HTTPS
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+       Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+      // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          // print server response payload
+          String payload = https.getString();
+          Serial.println(payload);
+          int temp = payload.toInt();
+          for (int i =1; i <= temp; i++) {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(250);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(250);
+          }
+
+        }
+      }
+      else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+      https.end();
+    }
+  }
+  else {
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }
+  Serial.println();
+  }
+
+}
